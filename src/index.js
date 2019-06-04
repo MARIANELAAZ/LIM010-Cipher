ee007b5c3fd15d4ddc2d2341db2ca8a103366b8e
MARIANELAAZ <marianela908@gmail.com>
2019-06-04 12:27:32 -0500

    entrada con laboratoria

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 /* Acá va tu código */
+const btn = document.getElementById('btn');
+const laboratoria = document.getElementById("contrasena");
+let sc1 = document.getElementById("screen-1");
+let sc2 = document.getElementById("screen-2");
+btn.addEventListener('click', entrar);
+
+function entrar() {
+  if (laboratoria.value ==="laboratoria") {
+    sc1.style.display = 'none';
+    sc2.style.display = 'block';
+  }else {
+    console.log("Incorrecto");
+  }
+};
