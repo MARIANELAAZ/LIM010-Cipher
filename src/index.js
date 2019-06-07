3f168498e0683a7c0da2f7341abc62b7f737926a
MARIANELAAZ <marianela908@gmail.com>
2019-06-07 08:44:12 -0500

    scream 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 /* Acá va tu código */
 const btn = document.getElementById('btn');
 const laboratoria = document.getElementById("contrasena");
-let sc1 = document.getElementById("screen-1");
-let sc2 = document.getElementById("screen-2");
-btn.addEventListener('click', entrar);
+const sc1 = document.getElementById("screen-1");
+const sc2 = document.getElementById("screen-2");
+const sc3 = document.getElementById("intento3")
+let numeroDeIntentos = 0;
 
-function entrar() {
-  if (laboratoria.value ==="laboratoria") {
+const entrar = () => {
+  btn.addEventListener('click', entrar);
+
+  if (laboratoria.value ==="LABORATORIA") {
     sc1.style.display = 'none';
     sc2.style.display = 'block';
-  }else {
-    console.log("Incorrecto");
-  }
+   }
+}
+ else {
+    sc1.style.display = 'block';
+    sc2.style.display = 'none'
+    document.getElementById('errorpass').innerHTML="Inténtalo nuevamente"
+    console.log("CORRECTO");
+    numeroDeIntentos++
+    if(numeroDeIntentos === 3) {
+      sc1.style.display = 'none';
+      sc2.style.display = 'none';
+    }
+
+}
+
 };
