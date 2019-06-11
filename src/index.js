ebe0007bb573df7cf6d1652459284fb8dd7cd363
MARIANELAAZ <marianela908@gmail.com>
2019-06-11 12:52:34 -0500

    cifrado

--- src/index.js
+++ src/index.js
@@ -26,3 +26,28 @@ else {
      }
 }
 });
+/* Cifrado */
+   let offset= document.getElementById("offset");
+   let tex1=document.getElementById("tex1");
+   let tex2=document.getElementById("tex2");
+   let buttonCifrado=document.getElementById("buttonCifrado");
+   let buttonDecifrado=document.getElementById("buttonDecifrado");
+
+   buttonCifrado.addEventListener("click", () => {
+     let offset1= offset.value;
+     let mensaje = tex1.value;
+     let mensajeFinal =  ""
+
+     for (let i= 0; i < mensaje.length; i++) {
+       let numeroAscii = mensaje.charCodeAt(i);
+       if (numeroAscii >=65  && numeroAscii <=90){
+       mensajeFinal += String.fromCharCode((numeroAscii-65+parseInt(offset1))%26+65);
+     }
+   else{
+     mensajeFinal+= tex2 [i]; /*espacio*/
+   }
+ }
+   tex2.value= mensajeFinal
+   });
+
+/*decifrado*/
