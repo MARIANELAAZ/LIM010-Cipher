07169d4169ce9ca0573da5af4ce7fe4ce4f984bd
MARIANELAAZ <marianela908@gmail.com>
2019-06-14 10:21:30 -0500

    proyecto final

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,37 @@
 window.cipher = {
-  encode: (offset, string) => {
-    /* Acá va tu código que cifra*/
-  },
-  decode: (offset, string) => {
-    /* Acá va tu código que descifra*/
+  encode: (offset1, mensaje) => {
+    let mensajeFinal =  "";
+    for (let i= 0; i < mensaje.length; i++) {
+      let numeroAscii = mensaje.charCodeAt(i);
+      if (numeroAscii >=65  && numeroAscii <=90){
+      mensajeFinal += String.fromCharCode((numeroAscii- 65 + parseInt(offset1)) %26 + 65);
+    }
+  else if(numeroAscii===32){
+    mensajeFinal += " ";
+    /*mensajeFinal+= mensaje[i]; /*espacio*/
   }
+}
+  return mensajeFinal;
+},
+
+  decode: (offset1, mensaje) => {
+    ///* Acá va tu código que descifra*/
+    let mensajeFinal =  "";
+    let caraCifrado = " ";
+    for (let i= 0; i < mensaje.length; i++) {
+     let numeroAscii = mensaje.charCodeAt(i);
+     if (numeroAscii >=65  && numeroAscii <=90) {
+        caraCifrado = (numeroAscii-65-parseInt(offset1)) % 26 + 65;/*devuelve valor númerico*/
+     if (caraCifrado <65) {
+        caraCifrado = caraCifrado+26;
+    }
+      mensajeFinal+= String.fromCharCode(caraCifrado);
+
+    } else {
+      mensajeFinal+= mensaje[i];
+    }
+    }
+
+  return mensajeFinal;
+}
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 /* Acá va tu código */
 const btn = document.getElementById("btn");
-const contraseña = document.getElementById("contraseña")
 const screen1 = document.getElementById("screen1");
 const screen2= document.getElementById("screen2");
 const screen3 = document.getElementById("screen3"); /*error pantalla*/
 let intentos = 0;
 /*hide esconder*/
 btn.addEventListener("click", () => {
-const contraseña = document.getElementById("contraseña")
-const password = contraseña.value
-  if(password ==="laboratoria") {
+const contraseña = document.getElementById("contraseña");
+const password = contraseña.value;
+  if(password ==="LABORATORIA") {
     screen1.classList.add("hide");
     screen2.classList.remove("hide");
     screen3.classList.add("hide");
@@ -28,26 +27,20 @@ else {
 });
 /* Cifrado */
    let offset= document.getElementById("offset");
-   let tex1=document.getElementById("tex1");
-   let tex2=document.getElementById("tex2");
-   let buttonCifrado=document.getElementById("buttonCifrado");
-   let buttonDecifrado=document.getElementById("buttonDecifrado");
+   let tex1= document.getElementById("tex1");
+   let tex2= document.getElementById("tex2");
+   let buttonCifrado= document.getElementById("buttonCifrado");
+   let buttonDecifrado= document.getElementById("buttonDecifrado");
 
    buttonCifrado.addEventListener("click", () => {
      let offset1= offset.value;
      let mensaje = tex1.value;
-     let mensajeFinal =  ""
-
-     for (let i= 0; i < mensaje.length; i++) {
-       let numeroAscii = mensaje.charCodeAt(i);
-       if (numeroAscii >=65  && numeroAscii <=90){
-       mensajeFinal += String.fromCharCode((numeroAscii-65+parseInt(offset1))%26+65);
-     }
-   else{
-     mensajeFinal+= tex2 [i]; /*espacio*/
-   }
- }
-   tex2.value= mensajeFinal
+     tex2.value= cipher.encode(offset1, mensaje);
    });
 
 /*decifrado*/
+buttonDecifrado.addEventListener("click", () => {
+  let offset1= offset.value;
+  let mensaje = tex1.value;
+  tex2.value= cipher.decode(offset1, mensaje);
+});
